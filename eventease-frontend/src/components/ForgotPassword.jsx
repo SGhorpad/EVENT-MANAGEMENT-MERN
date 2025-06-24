4401062904a0891b5a385a9866abc5d33ff2ee42
SGhorpad <sakshighorpade15@gmail.com>
2025-06-24 14:50:50 +0530

    Update ForgotPassword.jsx

--- eventease-frontend/src/components/ForgotPassword.jsx
+++ eventease-frontend/src/components/ForgotPassword.jsx
@@ -23,7 +23,7 @@ export default function ForgotPassword() {
   setLoading(true);
 
   try {
-    const res = await axios.post("http://localhost:5000/api/auth/forgot-password", { email });
+    const res = await axios.post("https://event-management-mern-1owy.onrender.com/api/auth/forgot-password", { email });
     setMsg(res.data.message);
     setEmail("");
   } catch (err) {
